f8ef63f1a549818cad9f1e76d20b7fc7767cc2bf
Łukasz Kapuściński <lukasz.kapuscinski@columbusglobal.com>
2022-07-07 17:53:54 +0200

    Add PaymentService abstractions

---
+++ Source/RevenueReportGenerator/Contract/IPayPalApi.cs
@@ -0,0 +1,9 @@
+﻿using Refit;
+
+namespace RevenueReportGenerator.Contract;
+
+internal interface IPayPalApi
+{
+    [Get("/v1/reporting/transactions")]
+    Task<string> GetTransactions(DateTime startDate, DateTime endDate);
+}
---
+++ Source/RevenueReportGenerator/IPaymentService.cs
@@ -0,0 +1,8 @@
+﻿using RevenueReportGenerator.DTO;
+
+namespace RevenueReportGenerator;
+
+internal interface IPaymentService
+{
+    Task<IEnumerable<TransactionDto>> GetTransactions(DateTime startDate, DateTime endDate);
+}
