4c2b6b96432695025b81a43d937163531192d563
lukkap <lukkap92@gmail.com>
2022-07-09 23:55:21 +0200

    Hnadle Transactions paging

--- Source/RevenueReportGenerator/Contract/PayPalTransactionsQueryParams.cs
+++ Source/RevenueReportGenerator/Contract/PayPalTransactionsQueryParams.cs
@@ -8,5 +8,10 @@ internal record PayPalTransactionsQueryParams
     public string StartDate { get; init; }
     [AliasAs("end_date")]
     public string EndDate { get; init; }
+    [AliasAs("fields")]
     public string Fields { get; init; }
+    [AliasAs("page_size")]
+    public int PageSize { get; set; }
+    [AliasAs("page")]
+    public int Page { get; set; }
 }
--- Source/RevenueReportGenerator/Services/PayPalService.cs
+++ Source/RevenueReportGenerator/Services/PayPalService.cs
@@ -8,6 +8,7 @@ namespace RevenueReportGenerator.Services;
 internal class PayPalService : IPaymentService
 {
     private readonly IPayPalApi _payPalApi;
+
     private const string CompletedStatus = "S";
 
     public PayPalService(IPayPalApi payPalApi)
@@ -17,22 +18,19 @@ internal class PayPalService : IPaymentService
 
     public async Task<IEnumerable<TransactionInfoDto>> GetRevenueTransactions(DateTime startDate, DateTime endDate)
     {
-        // TODO: Handle paging
         var queryParams = new PayPalTransactionsQueryParams
         {
             StartDate = startDate.ToPayPalFormat(),
             EndDate = endDate.ToPayPalFormat(),
-            Fields = "all"
+            Fields = "all",
+            PageSize = 100,
+            Page = 1
         };
 
-        var responseString = await _payPalApi.GetTransactions(queryParams);
+        var transactions = await GetTransactions(queryParams).ToListAsync();
 
-        var transaction = JsonConvert.DeserializeObject<TransactionDto>(responseString,
-            new JsonSerializerSettings {
-                ContractResolver = new DefaultContractResolver { NamingStrategy = new SnakeCaseNamingStrategy() }
-            });
-
-        var revenueTransactions = transaction?.TransactionDetails
+        var revenueTransactions = transactions
+            .SelectMany(tds => tds)
             .Select(td => td.TransactionInfo)
             .Where(ti => ti.Amount?.Value > 0 &&
                          ti.Subject is not null &&
@@ -40,5 +38,25 @@ internal class PayPalService : IPaymentService
             .ToList() ?? Enumerable.Empty<TransactionInfoDto>();
 
         return revenueTransactions;
+    }
+
+    private async IAsyncEnumerable<IEnumerable<TransactionDetailsDto>> GetTransactions(PayPalTransactionsQueryParams queryParams)
+    {
+        do
+        {
+            var responseString = await _payPalApi.GetTransactions(queryParams);
+            var response = JsonConvert.DeserializeObject<TransactionDto>(responseString,
+                new JsonSerializerSettings {
+                    ContractResolver = new DefaultContractResolver { NamingStrategy = new SnakeCaseNamingStrategy() }
+                });
+
+            var transactionDetails = (response?.TransactionDetails ?? Enumerable.Empty<TransactionDetailsDto>()).ToList();
+            yield return transactionDetails;
+
+            if (queryParams.Page == response?.TotalPages || !transactionDetails.Any())
+                break;
+
+            queryParams.Page++;
+        } while (true);
     }
 }
